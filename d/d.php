2a6ff573e02c264e97c83700f219e86e9fc15557
dajamplay <ggyoug@gmail.cpm>
2021-05-22 13:10:20 +0300

    D

--- d/d.php
+++ d/d.php
@@ -1,2 +1,72 @@
 <?php
 
+// Принцип инверсии зависимостей
+// dependency inversion principle
+// Зависимость на Абстракциях. Нет зависимости на что-то конкретное
+
+// Зависимости строятся на абстракциях, а не на классах
+
+class LowRankingMale {
+    public function eat() {
+        $wife = new Wife();
+        $food = $wife->getFood();
+    }
+}
+
+class AverageRankingMale {
+
+    private Wife $wife;
+
+    public function __construct(Wife $wife)
+    {
+        $this->wife = $wife;
+    }
+
+    public function eat() {
+        $food = $this->wife->getFood();
+    }
+}
+
+class HighRankingMale {
+    private IFoodProvider $foodProvider;
+
+    public function __construct(IFoodProvider $foodProvider)
+    {
+        $this->foodProvider = $foodProvider;
+    }
+
+    public function eat() {
+        $food = $this->foodProvider->getFood();
+        echo $food;
+    }
+
+}
+
+class Wife implements IFoodProvider {
+
+    public function getFood() {
+        return 'wife food';
+    }
+
+}
+
+class Restaurant implements IFoodProvider {
+
+    public function getFood()
+    {
+        return 'restaurant food';
+    }
+
+}
+
+interface IFoodProvider {
+
+    public function getFood();
+
+}
+
+$food = new Restaurant();
+
+$male = new HighRankingMale($food);
+
+$male->eat();
